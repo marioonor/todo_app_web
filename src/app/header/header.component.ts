cd8955f6a39b5bcc474fb1d223d1c89b126b5a23
MarioOnor <101718964+MarioOnor@users.noreply.github.com>
2025-06-18 16:21:04 +0800

    fixed the username to appear on the header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,26 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from '../service/auth.service';
 import { Router } from '@angular/router';
-import { TodoService } from '../service/todo.service';
-
+import { UserResponse } from '../models/auth.models';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
   imagePath: string = 'assets/images/image.png';
   imageIconPath: string = 'assets/images/usericon.png';
   imageLogoutPath: string = 'assets/images/logout.png';
-  userFirstName: string = "Hello, Angel";
+  userFirstName: string = 'User';
+  private userAuthSubscription: Subscription | undefined;
 
-  constructor(
-      private router: Router,
-      private todoService: TodoService,
-      private authService: AuthService
-    ) {}
+  constructor(private router: Router, private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.userAuthSubscription = this.authService.currentUser.subscribe(
+      (user: UserResponse | null) => {
+        if (user && user.username) {
+          this.userFirstName = user.username;
+        } else {
+          this.userFirstName = 'User';
+        }
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    if (this.userAuthSubscription) {
+      this.userAuthSubscription.unsubscribe();
+    }
+  }
 
   logout() {
     this.authService.logout();
