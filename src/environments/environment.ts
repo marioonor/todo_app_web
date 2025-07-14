13b04887948ea9f32d4a0badc75d979120241a22
Mario <101718964+marioonor@users.noreply.github.com>
2025-07-14 09:15:28 +0800

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://34.238.44.249:8080' // Base API URL for development
   apiUrl: 'http://localhost:8080' // Base API URL for development
 };
